b53273d0610cd7ad7b7372e512d87c848be0c292
TheTuso <piotrekpasztor@gmail.com>
2025-02-20 11:38:36 +0100

    Generate c3 vouchers

--- internal/services/csrvclient.go
+++ internal/services/csrvclient.go
@@ -1,13 +1,17 @@
 package services
 
 import (
+	"bytes"
 	"context"
+	"csrvbot/domain/entities"
 	"csrvbot/pkg/logger"
 	"encoding/json"
 	"fmt"
+	"github.com/Craftserve/monies"
 	"io"
 	"math/rand"
 	"net/http"
+	"time"
 )
 
 type CsrvClient struct {
@@ -20,10 +24,6 @@ func NewCsrvClient(secret, environment, craftserveUrl string) *CsrvClient {
 	return &CsrvClient{Secret: secret, Environment: environment, CraftserveUrl: craftserveUrl}
 }
 
-type VoucherResponse struct {
-	Code string `json:"code"`
-}
-
 func (c *CsrvClient) GetCSRVCode(ctx context.Context) (string, error) {
 	log := logger.GetLoggerFromContext(ctx)
 	log.Debug("Generating CSRV voucher")
@@ -32,23 +32,50 @@ func (c *CsrvClient) GetCSRVCode(ctx context.Context) (string, error) {
 		return fmt.Sprintf("DEV-%d", rand.Int()), nil
 	}
 
-	req, err := http.NewRequest("POST", fmt.Sprintf("%s/api/generate_voucher", c.CraftserveUrl), nil)
+	prefix, group := "discord", "discord-giveaway"
+	expires := time.Now().Add(365 * 24 * time.Hour)
+	uses := 1
+	payload := entities.GenerateVoucherPayload{
+		Length:  16,
+		Charset: "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+		Prefix:  &prefix,
+		Expires: &expires,
+		GroupId: &group,
+		Uses:    &uses,
+		PerUser: &uses,
+		Actions: []entities.VoucherAction{
+			{
+				WalletTx: map[monies.CurrencyCode]monies.Money{
+					monies.PLN: monies.MustNew(500, monies.PLN),
+				},
+			},
+		},
+	}
+
+	bodyPayload := new(bytes.Buffer)
+	err := json.NewEncoder(bodyPayload).Encode(payload)
+	if err != nil {
+		return "", fmt.Errorf("GetCSRVCode json.NewEncoder failed: %w", err)
+	}
+
+	req, err := http.NewRequest("POST", fmt.Sprintf("%s/api/admin/voucher/generate", c.CraftserveUrl), bodyPayload)
 	if err != nil {
 		return "", err
 	}
-	req.SetBasicAuth("csrvbot", c.Secret)
+
+	req.AddCookie(&http.Cookie{Name: "user_access_token", Value: c.Secret})
 
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
 		return "", err
 	}
 
-	var code VoucherResponse
+	var voucher entities.PublicVoucher
 	bodyBytes, err := io.ReadAll(resp.Body)
 	if err != nil {
 		return "", err
 	}
-	err = json.Unmarshal(bodyBytes, &code)
+	err = json.Unmarshal(bodyBytes, &voucher)
 	if err != nil {
 		return "", fmt.Errorf("getCSRVCode json.Unmarshal failed: %w with body: %s", err, string(bodyBytes))
 	}
@@ -56,5 +83,5 @@ func (c *CsrvClient) GetCSRVCode(ctx context.Context) (string, error) {
 	if err != nil {
 		log.WithError(err).Error("getCSRVCode resp.Body.Close()")
 	}
-	return code.Code, nil
+	return voucher.Id, nil
 }
