b467c0e167725c1510f5830883058ea709860dcd
Piotr Pasztor <86690614+TheTuso@users.noreply.github.com>
2025-06-16 13:47:54 +0200

    refactor(vouchers): handle vouchers array (#16)

--- dtos/voucher.go
+++ dtos/voucher.go
@@ -6,12 +6,13 @@ import (
 )
 
 type GenerateVoucherPayload struct {
-	Length  int                      `json:"length"`
-	Charset string                   `json:"charset"`
-	Prefix  *string                  `json:"prefix"`
-	Expires *time.Time               `json:"expires"`
-	GroupId *string                  `json:"group_id"`
-	Uses    *int                     `json:"uses"`
-	PerUser *int                     `json:"per_user"`
-	Actions []entities.VoucherAction `json:"actions"`
+	Length   int                      `json:"length"`
+	Charset  string                   `json:"charset"`
+	Prefix   *string                  `json:"prefix"`
+	Expires  *time.Time               `json:"expires"`
+	GroupId  *string                  `json:"group_id"`
+	Uses     *int                     `json:"uses"`
+	PerUser  *int                     `json:"per_user"`
+	Actions  []entities.VoucherAction `json:"actions"`
+	Quantity *int                     `json:"quantity"`
 }
